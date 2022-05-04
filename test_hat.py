57226497c5349900a22c1d5682e56c0c6d192a08
cdbahl <cdbahl@gmail.com>
2022-05-04 13:48:36 -0400

    updated test_hat.py

--- test_hat.py
+++ test_hat.py
@@ -1,17 +1,17 @@
 from sense_hat import SenseHat
 sense = SenseHat()
-#sense.show_message("I love Jessica!")
+sense.show_message("Lorem ipsum")
 sense.clear()
 
-pressure = sense.get_pressure()
-print(f"pressure {pressure}")
-
-tempp = sense.get_temperature_from_pressure()
-print(f"temp from pressure {tempp}")
-
-temph = sense.get_temperature_from_humidity()
-print(f"temp from humidity {temph}")
-
-humidity = sense.get_humidity()
-print(f"humidity {humidity}")
+#pressure = sense.get_pressure()
+#print(f"pressure {pressure}")
+#
+#tempp = sense.get_temperature_from_pressure()
+#print(f"temp from pressure {tempp}")
+#
+#temph = sense.get_temperature_from_humidity()
+#print(f"temp from humidity {temph}")
+#
+#humidity = sense.get_humidity()
+#print(f"humidity {humidity}")
 
